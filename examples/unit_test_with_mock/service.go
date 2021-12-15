48b3671846a03f0c1b670ab1560e5adee7d8eab0
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2021-12-15 20:58:00 +0100

    Add example of unit tests with mock grafik client (#53)
    * Add example of unit tests with mock grafik client

---
+++ examples/unit_test_with_mock/service.go
@@ -0,0 +1,42 @@
+package unit_test_example_with_mock
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+type service struct {
+	client SpaceXClient
+}
+
+func (s service) ReturnAverageCostForPerLaunch() (int, error) {
+	res, err := s.client.GetRocketResults(50, nil)
+	if err != nil {
+		return 0, fmt.Errorf("SpaceXClient failed: %s", err.Error())
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(res.Body)
+
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return 0, err
+	}
+	var graphqlRes GetRocketResultsResponse
+	err = json.Unmarshal(b, &graphqlRes)
+	if err != nil {
+		return 0, err
+	}
+
+	numOfMissions := len(graphqlRes.Data.RocketsResult.Data)
+	totalCost := 0
+	for _, rocket := range graphqlRes.Data.RocketsResult.Data {
+		totalCost += rocket.CostPerLaunch
+	}
+	return totalCost / numOfMissions, nil
+}
---
+++ examples/unit_test_with_mock/spacex_client.go
@@ -0,0 +1,82 @@
+// Generated with grafik. DO NOT EDIT
+
+package unit_test_example_with_mock
+
+import (
+	GraphqlClient "github.com/Bartosz-D3V/grafik/client"
+	"net/http"
+)
+
+type RocketsResult struct {
+	Result Result   `json:"result"`
+	Data   []Rocket `json:"data"`
+}
+
+type Result struct {
+	TotalCount int `json:"totalCount"`
+}
+
+type Rocket struct {
+	CostPerLaunch int    `json:"cost_per_launch"`
+	Country       string `json:"country"`
+	Name          string `json:"name"`
+}
+
+const getRocketResults = `query getRocketResults($limit: Int){
+    rocketsResult(limit: $limit) {
+        result {
+            totalCount
+        }
+        data {
+            name
+            country
+            cost_per_launch
+        }
+    }
+}
+`
+
+type SpaceXClient interface {
+	GetRocketResults(limit int, header *http.Header) (*http.Response, error)
+}
+
+func (c *spaceXClient) GetRocketResults(limit int, header *http.Header) (*http.Response, error) {
+	params := make(map[string]interface{}, 1)
+	params["limit"] = limit
+
+	return c.ctrl.Execute(getRocketResults, params, header)
+}
+
+type GetRocketResultsResponse struct {
+	Data   GetRocketResultsData `json:"data"`
+	Errors []GraphQLError       `json:"errors"`
+}
+
+type GetRocketResultsData struct {
+	RocketsResult RocketsResult `json:"rocketsResult"`
+}
+
+type GraphQLError struct {
+	Message    string                 `json:"message"`
+	Locations  []GraphQLErrorLocation `json:"locations"`
+	Extensions GraphQLErrorExtensions `json:"extensions"`
+}
+
+type GraphQLErrorLocation struct {
+	Line   int `json:"line"`
+	Column int `json:"column"`
+}
+
+type GraphQLErrorExtensions struct {
+	Code string `json:"code"`
+}
+
+type spaceXClient struct {
+	ctrl GraphqlClient.Client
+}
+
+func New(endpoint string, client *http.Client) SpaceXClient {
+	return &spaceXClient{
+		ctrl: GraphqlClient.New(endpoint, client),
+	}
+}
