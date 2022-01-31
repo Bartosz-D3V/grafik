1d9cf4545ac02888e789ff7dfde1af67e4954f4a
Bartosz-D3V <nix.noxi379@gmail.com>
2022-01-31 17:43:51 +0100

    Add __typename support
    https://github.com/Bartosz-D3V/grafik/issues/71
    
    * Add tests

--- generator/type_test.go
+++ generator/type_test.go
@@ -56,6 +56,57 @@ func TestTypeArg_ExportType(t *testing.T) {
 	}
 }
 
+func TestTypeField_ExportName(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		t   TypeField
+		exp string
+	}{
+		{TypeField{Name: "test"}, "Test"},
+		{TypeField{Name: "Test"}, "Test"},
+		{TypeField{Name: "1test"}, "1test"},
+		{TypeField{Name: "1Test"}, "1Test"},
+		{TypeField{Name: ""}, ""},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.exp, test.t.ExportName())
+	}
+}
+
+func TestTypeField_ExportType(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		t   TypeField
+		exp string
+	}{
+		{TypeField{Type: "string"}, "string"},
+		{TypeField{Type: "int"}, "int"},
+		{TypeField{Type: "bool"}, "bool"},
+		{TypeField{Type: "[]string"}, "[]string"},
+		{TypeField{Type: "[]int"}, "[]int"},
+		{TypeField{Type: "[]bool"}, "[]bool"},
+		{TypeField{Type: "[][]string"}, "[][]string"},
+		{TypeField{Type: "[][]int"}, "[][]int"},
+		{TypeField{Type: "[][]bool"}, "[][]bool"},
+		{TypeField{Type: "[][][]string"}, "[][][]string"},
+		{TypeField{Type: "[][][]int"}, "[][][]int"},
+		{TypeField{Type: "[][][]bool"}, "[][][]bool"},
+		{TypeField{Type: "person"}, "Person"},
+		{TypeField{Type: "[]person"}, "[]Person"},
+		{TypeField{Type: "[][]person"}, "[][]Person"},
+		{TypeField{Type: "[][][]person"}, "[][][]Person"},
+		{TypeField{Type: "Person"}, "Person"},
+		{TypeField{Type: "[]Person"}, "[]Person"},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.exp, test.t.ExportType().Type)
+	}
+}
+
 func TestTypeField_PointerType(t *testing.T) {
 	t.Parallel()
 
